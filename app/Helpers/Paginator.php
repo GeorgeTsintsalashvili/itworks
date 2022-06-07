6e3fe08193237e89941f2e121c05b86301974509
GeorgeTsintsalashvili <44457851+GeorgeTsintsalashvili@users.noreply.github.com>
2022-06-07 23:10:49 +0400

    Update Paginator.php

--- app/Helpers/Paginator.php
+++ app/Helpers/Paginator.php
@@ -18,31 +18,31 @@ class Paginator
        $object -> currentPage = $curPage <= $object -> maxPage ? $curPage : $object -> maxPage;
 
        $curPage = $object -> currentPage;
- 	     $rightBoundaryLastNum = $object -> currentPage + $adjacent;
- 	     $leftBoundaryLastNum = $object -> currentPage - $adjacent;
+ 	   $rightBoundaryLastNum = $object -> currentPage + $adjacent;
+ 	   $leftBoundaryLastNum = $object -> currentPage - $adjacent;
 
- 	     if (($leftBoundaryLastNum - $leftBoundary) > 1)
- 	     {
- 	       for ($i = 1; $i <= $leftBoundary; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
+ 	   if (($leftBoundaryLastNum - $leftBoundary) > 1)
+ 	   {
+ 	     for ($i = 1; $i <= $leftBoundary; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
 
- 	       $object -> pages[] = ['value' => $object -> pad, 'isPad' => true, 'isActive' => false];
+ 	     $object -> pages[] = ['value' => $object -> pad, 'isPad' => true, 'isActive' => false];
 
- 	       for ($i = $leftBoundaryLastNum; $i <= $curPage; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
- 	     }
+ 	     for ($i = $leftBoundaryLastNum; $i <= $curPage; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
+ 	   }
 
- 	     if (($leftBoundaryLastNum >= 1 && $leftBoundaryLastNum <= $leftBoundary) || $leftBoundaryLastNum <= 0 || ($leftBoundaryLastNum - $leftBoundary) == 1)
-         {
- 	       for ($i = 1; $i <= $curPage; $i++) $object -> pages[] =  ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
-         }
+ 	   if (($leftBoundaryLastNum >= 1 && $leftBoundaryLastNum <= $leftBoundary) || $leftBoundaryLastNum <= 0 || ($leftBoundaryLastNum - $leftBoundary) == 1)
+       {
+ 	     for ($i = 1; $i <= $curPage; $i++) $object -> pages[] =  ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
+       }
 
- 	     if (($rightBoundaryLastNum + 2) <= $object -> maxPage - $rightBoundary)
- 	     {
- 	       for ($i = $curPage + 1; $i <= $rightBoundaryLastNum; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
+ 	   if (($rightBoundaryLastNum + 2) <= $object -> maxPage - $rightBoundary)
+ 	   {
+ 	     for ($i = $curPage + 1; $i <= $rightBoundaryLastNum; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
 
- 	       $object -> pages[] = ['value' => $object -> pad, 'isPad' => true, 'isActive' => false];
+ 	     $object -> pages[] = ['value' => $object -> pad, 'isPad' => true, 'isActive' => false];
 
- 	       for ($i = $object -> maxPage - $rightBoundary; $i <= $object -> maxPage; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
- 	     }
+ 	     for ($i = $object -> maxPage - $rightBoundary; $i <= $object -> maxPage; $i++) $object -> pages[] = ['value' => $i, 'isPad' => false, 'isActive' => $object -> currentPage == $i];
+ 	   }
 
        if (($object -> maxPage - $rightBoundary) - $rightBoundaryLastNum == 1 || $rightBoundaryLastNum >= $object -> maxPage - $rightBoundary)
        {
